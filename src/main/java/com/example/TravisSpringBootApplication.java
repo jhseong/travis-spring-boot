0df68214575590210a7389bcbc6e9dd1c5e595b7
Estel Seong <estel.castle@gmail.com>
2016-07-26 01:04:43 +0900

    project setting
    .gitignore 추가

---
+++ src/main/java/com/example/TravisSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisSpringBootApplication.class, args);
+	}
+}
